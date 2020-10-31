c920b84ca73d2a15dcf790a83482956212d81bc7
Rany Keddo <purzelrakete@gmail.com>
2020-10-31 13:52:42 +1300

    working audio training

--- test_train.py
+++ test_train.py
@@ -4,3 +4,8 @@ from wavenet import train
 def test_trainer_params():
     p = train.HParams()
     assert p.batch_size == 64
+
+
+def test_trainer_params_override():
+    p = train.HParams(batch_size=1)
+    assert p.batch_size == 1
